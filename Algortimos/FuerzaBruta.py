7841c51c3b509378a4d3d42e29f35be646c41d60
Vynidaulkel <kelvinnunezc@hotmail.com>
2021-09-22 01:07:05 -0600

    Terminado algoritmo creador de soluciones

--- Algortimos/FuerzaBruta.py
+++ Algortimos/FuerzaBruta.py
@@ -8,12 +8,16 @@ def FuerzaBruta(n):
 def CrearPermutaciones(A,n, pArregloSolucion):
     if n==0:
         #print(A) #cambiar a que retorne un arreglo ([[pos1],[pos2]])
-        pArregloSolucion.append(A)
+        b = A.copy()
+        print(A)
+        pArregloSolucion.append(b)
     else:
         A[n-1] = 0
         CrearPermutaciones(A, n-1, pArregloSolucion)
         A[n-1] = 1
         CrearPermutaciones(A, n-1, pArregloSolucion)
 
+def busqueda():
+    return
 
-FuerzaBruta(5)
+FuerzaBruta(3)
